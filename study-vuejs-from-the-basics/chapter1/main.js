942bf1306caf8b856131bcdc6d763a0a27119d38
kakerukaneko <kaneko@beenos.com>
2020-11-22 16:11:21 +0900

    done: CHAPTER 1

--- study-vuejs-from-the-basics/chapter1/main.js
+++ study-vuejs-from-the-basics/chapter1/main.js
@@ -1,3 +1,13 @@
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    message: 'Hello, Vue.js!',
+    list: ['リンゴ', 'バナナ', 'イチゴ'],
+    show: true
+  },
+  methods: {
+    handleClick: function(event) {
+      alert(event.target)
+    }
+  }
 })
